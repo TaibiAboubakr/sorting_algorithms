5bc349dc74f6a33a4e4d230a5e2d6f18a9ccb806
nisselhbb <nissrineelhobbi@gmail.com>
2023-10-24 15:43:45 -0700

    a function that sorts an array of integers in ascending order using the Radix sort algorithm

---
+++ 105-radix_sort.c
@@ -0,0 +1,17 @@
+#include "sort.h"
+/**
+ * radix_sort -  sorts an array of integers in ascending order
+ * using the radix sort algorithm
+ * @array: pointer to the array to be printed
+ * @size: number of the elements in the array
+ * Return: no return
+ */
+
+
+void radix_sort(int *array, size_t size)
+{
+
+	if (!array || size < 2)
+		return;
+
+}
