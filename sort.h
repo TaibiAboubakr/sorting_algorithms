b3bb4a495eb68408dfe0b8503f65e9822f6ee75b
aboubakr <c.aboubakr@gmail.com>
2023-10-24 23:16:01 +0100

    uadding a new prototypes for implemented functions

--- sort.h
+++ sort.h
@@ -41,5 +41,7 @@ void quick_sort_hoare(int *array, size_t size);
 int hoare_partition(int *array, int low, int high, size_t size);
 void cocktail_sort_list(listint_t **list);
 void swap_nodes1(listint_t **list, listint_t *node1, listint_t *node2);
+void heap_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 
 #endif /* SORT_H */
